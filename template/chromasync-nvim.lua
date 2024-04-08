9efba3ffc10671fb79cff62cc1f28c3fc1a0e304
daniele <daniele.monzani@outlook.com>
2024-04-08 12:58:04 +0200

    feat(colors): add bg/fg composite colors

--- template/chromasync-nvim.lua
+++ template/chromasync-nvim.lua
@@ -28,4 +28,16 @@ return {
   magh = "{MAGH}",
   cynh = "{CYNH}",
   whth = "{WHTH}",
+
+  bg10fg = "{BG:10:FG}",
+  bg20fg = "{BG:20:FG}",
+  bg30fg = "{BG:30:FG}",
+  bg40fg = "{BG:40:FG}",
+  bg50fg = "{BG:50:FG}",
+
+  fg10bg = "{FG:10:BG}",
+  fg20bg = "{FG:20:BG}",
+  fg30bg = "{FG:30:BG}",
+  fg40bg = "{FG:40:BG}",
+  fg50bg = "{FG:50:BG}",
 }
