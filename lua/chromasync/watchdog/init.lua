10809eabecaa734ef0591792e0ae394cd2135b82
daniele <daniele.monzani@outlook.com>
2024-04-15 10:30:42 +0200

    feat(watchdog): add timer

--- lua/chromasync/watchdog/init.lua
+++ lua/chromasync/watchdog/init.lua
@@ -1,12 +1,28 @@
 local M = {}
-local id = nil
 
 M.setup = function()
+	local timer = vim.loop.new_timer()
+
+	if timer == nil then
+		vim.notify_once("Unable to start the watchdog timer", vim.log.levels.ERROR)
+		return nil
+	end
+
+	-- Starts the timer
+	timer:start(1000, 1000, function()
+	end)
+
+	-- Starts the autocmd to stop the timer once the colorscheme changes
+	local id = nil
 	id = vim.api.nvim_create_autocmd("ColorSchemePre", {
 		callback = function(ev)
-			if ev.match ~= "chromasync" and id ~= nil then
-				print("DEL" .. string.format('event fired: %s', vim.inspect(ev)))
-				vim.api.nvim_del_autocmd(id)
+			if ev.match ~= "chromasync" then
+				-- Stops the timer and deletes the autocmd
+				timer:stop()
+
+				if id ~= nil then
+					vim.api.nvim_del_autocmd(id)
+				end
 			end
 		end
 	})
