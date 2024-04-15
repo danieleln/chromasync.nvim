fc6a51bde32470acf6bd0bc9e78ba8e2b457192e
daniele <daniele.monzani@outlook.com>
2024-04-15 16:36:53 +0200

    fix: store template in chormasync dir
    `/tmp/chormasync.nvim` is not monitored by chromasync. Changing theme
    doesn't update chromasync.nvim template.

--- lua/chromasync/config.lua
+++ lua/chromasync/config.lua
@@ -1,14 +1,17 @@
 local util = require("chromasync.util")
 local M = {}
 
-local TMP_DIR = "/tmp/chromasync.nvim/"
+local HOME = vim.loop.os_homedir() .. "/"
+
+local CHROMASYNC_CONFIG_DIR = os.getenv("CHROMASYNC_CONFIG_DIR") or HOME .. ".config/chromasync/"
+local CHROMASYNC_CACHE_DIR  = os.getenv("CHROMASYNC_CACHE_DIR")  or HOME .. ".cache/chromasync/"
 
 M.FILE_NAME = "chromasync-nvim.json"
 
-M.CHROMASYNC_TEMPLATES_DIR = TMP_DIR .. "template/"
+M.CHROMASYNC_TEMPLATES_DIR = CHROMASYNC_CONFIG_DIR .. "templates/"
 M.CHROMASYNC_TEMPLATE_FILE = M.CHROMASYNC_TEMPLATES_DIR .. M.FILE_NAME
 
-M.CHROMASYNC_OUT_DIR = TMP_DIR
+M.CHROMASYNC_OUT_DIR  = CHROMASYNC_CACHE_DIR .."out/"
 M.CHROMASYNC_OUT_FILE = M.CHROMASYNC_OUT_DIR .. M.FILE_NAME
 
 -- Builds the directories
